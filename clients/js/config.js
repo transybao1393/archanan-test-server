24d3dc8e8f938102773cccd82c05210bcc2d4597
Admin <Admin@Johnathans-Mac.local>
2020-08-12 11:41:39 +0700

    remove client folder

--- clients/js/config.js
+++
@@ -1,7 +0,0 @@
-window._config = {
-    cognito: {
-        userPoolId: 'ap-southeast-1_ihWPvETFg', // e.g. us-east-2_uXboG5pAb
-        region: 'ap-southeast-1', // e.g. us-east-2
-		clientId: '5mhq4tjs8nuscdjr1cee65ev9i' //is this used anywhere?
-    },
-};
